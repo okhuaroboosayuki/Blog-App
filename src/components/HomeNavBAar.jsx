2ddc2410bf2399963ad31cf470c33260cdb88496
okhuarobo osayuki <osayukiraymond@gmail.com>
2023-04-28 16:35:05 +0100

    added icons from material ui icons, & created a responsive navigation menu

--- src/components/HomeNavBAar.jsx
+++ src/components/HomeNavBAar.jsx
@@ -1,35 +1,87 @@
-import '../assets/css/navbar.css';
-import SearchIcon from '@mui/icons-material/Search';
+import "../assets/scss/navbar.scss";
+import SearchIcon from "@mui/icons-material/Search";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import MenuIcon from "@mui/icons-material/Menu";
+import CloseIcon from '@mui/icons-material/Close';
 
 function HomeNavBar() {
   return (
-    <nav className="nav-bar">
+    <nav className="nav_bar">
       <h1 className="logo">
         <a href="#">Blog</a>
       </h1>
-      <ul className="nav-list">
-        <a href="#" className="nav-item">
-          Home
-        </a>
-        <a href="#" className="has-children nav-item">
-          Category
-        </a>
-        <a href="#" className="has-children nav-item">
-          Blog
-        </a>
-        <a href="#" className="nav-item">
-          About
-        </a>
-        <a href="#" className="nav-item">
-          Contact
-        </a>
-      </ul>
 
-      {/* <>{searchState.search}</> */}
-      {/* Search input placed above the search icon */}
+      <div className="nav_list">
+        <ul>
+          <li>
+            <a href="#" className="nav_item">
+              Home
+            </a>
+          </li>
+          <li className="has_children">
+            <a href="#" className="nav_item">
+              Categories
+            </a>
+            <KeyboardArrowDownIcon className="arrow_down" />
+          </li>
+          <li className="has_children">
+            <a href="#" className="nav_item">
+              Blog
+            </a>
+            <KeyboardArrowDownIcon className="arrow_down" />
+          </li>
+          <li>
+            <a href="#" className="nav_item">
+              About
+            </a>
+          </li>
+          <li>
+            <a href="#" className="nav_item">
+              Contact
+            </a>
+          </li>
+        </ul>
 
-      <div className="nav-search">
-        <SearchIcon className="search-icon" />
+        {/* <>{searchState.search}</> */}
+        {/* Search input placed above the search icon */}
+      </div>
+
+      <div className="nav_extras">
+        <SearchIcon className="search_icon" />
+        <MenuIcon className="mobile_menu_bar" />
+      </div>
+
+      <div className="mobile_menu">
+        <CloseIcon className="close_icon" />
+        <ul>
+          <li>
+            <a href="#" className="nav_item">
+              Home
+            </a>
+          </li>
+          <li className="mobile_menu_has_children">
+            <a href="#" className="nav_item">
+              Categories
+            </a>
+            <KeyboardArrowDownIcon className="arrow_down" />
+          </li>
+          <li className="mobile_menu_has_children">
+            <a href="#" className="nav_item">
+              Blog
+            </a>
+            <KeyboardArrowDownIcon className="arrow_down" />
+          </li>
+          <li>
+            <a href="#" className="nav_item">
+              About
+            </a>
+          </li>
+          <li>
+            <a href="#" className="nav_item">
+              Contact
+            </a>
+          </li>
+        </ul>
       </div>
     </nav>
   );
