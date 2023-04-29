689d4dbd37a4f0b29280743d9f512fdb173df211
okhuarobo osayuki <osayukiraymond@gmail.com>
2023-04-29 23:54:03 +0100

    removed unused codes, & added links to footer bottom_right links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -10,104 +10,6 @@ function Footer() {
   return (
     <footer className="footer_container">
       <Newsletter />
-
-      {/* <div className="footer_bottom">
-        <div className="footer_bottom_left">
-          <div>
-            <h3 className="title">Vitality Avenue</h3>
-            <p className="footer_bottom_left_paragraph">
-              Welcome to Vitality Avenue, the ultimate destination for health
-              and lifestyle enthusiasts. Our blog provides expert advice and
-              practical tips on nutrition, fitness, mental wellness, and more.
-              Discover new ways to boost your energy, improve your well-being,
-              and live your best life. Join our community today and start your
-              journey towards optimal health and vitality.
-            </p>
-
-            <ul className="social_links">
-              <li>
-                <a href="#">
-                  <FacebookRoundedIcon className="social_link"/>
-                </a>
-              </li>
-              <li>
-                <a href="#">
-                    <TwitterIcon className="social_link"/>
-                </a>
-              </li>
-              <li>
-                <a href="#">
-                    <InstagramIcon className="social_link"/>
-                </a>
-              </li>
-              <li>
-                <a href="#">
-                    <PinterestIcon className="social_link"/>
-                </a>
-              </li>
-            </ul>
-        </div>
-          </div>
-
-
-        <div className="footer_bottom_right">
-          <div className="footer_bottom_right_top">
-            <div className="footer_bottom_right_top-left_side">
-              <h3 className="title">categories</h3>
-              <ul className="categories">
-                <li className="category">
-                  <a href="#">category 1</a>
-                </li>
-                <li className="category">
-                  <a href="#">category 2</a>
-                </li>
-                <li className="category">
-                  <a href="#">category 3</a>
-                </li>
-                <li className="category">
-                  <a href="#">category 4</a>
-                </li>
-              </ul>
-            </div>
-
-            <div className="footer_bottom_right_top-right_side">
-              <h3 className="title">site links</h3>
-              <ul className="links">
-                <li className="link">
-                  <a href="#">home</a>
-                </li>
-                <li className="link">
-                  <a href="#">blog</a>
-                </li>
-                <li className="link">
-                  <a href="#">about</a>
-                </li>
-                <li className="link">
-                  <a href="#">contact</a>
-                </li>
-              </ul>
-            </div>
-          </div>
-
-          <div className="footer_bottom_right_bottom">
-            <ul>
-              <li className="copyright">
-              <CopyrightRoundedIcon className="copyright_icon" />
-            copyright {new Date().getFullYear()}</li>
-              <li>
-                <span>
-                  design by <a href="#">styleshout</a>
-                </span>
-              </li>
-              <li>
-                <span>
-                  developed by <a href="#">osayuki</a>
-                </span>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </div> */}
       <div className="footer_bottom">
         <div className="top">
           <div className="top_left">
@@ -190,13 +92,13 @@ function Footer() {
               </li>
               <li>
                 <span>
-                  design by <a href="#">styleshout</a>
+                  design by <a href="https://styleshout.com/" target="blank">styleshout</a>
                 </span>
               </li>
             </ul>
             <div className="dev">
               <span>
-                developed by <a href="#">osayuki</a>
+                developed by <a href="https://github.com/okhuaroboosayuki" target="blank">osayuki</a>
               </span>
             </div>
           </div>
