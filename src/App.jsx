4c270c62609e17e80a65dfe6b0a0134f0df2130a
okhuarobo osayuki <osayukiraymond@gmail.com>
2023-04-29 01:17:52 +0100

    imported Footer component, & added div blocks around the components

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
 import "./assets/css/style.css";
-import HomeNavBar from "./components/HomeNavBAar";
+import HomeNavBar from "./components/HomeNavBar";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-    <>
-      <HomeNavBar />
-    </>
-  )
+    <div className="home_page">
+      <div className="nav">
+        <HomeNavBar />
+      </div>
+
+      <div className="content">
+        content goes here
+      </div>
+
+      <div className="footer">
+        <Footer />
+      </div>
+    </div>
+  );
 }
 
 export default App;
