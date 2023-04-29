3dd227aab53a764b70841db7ddad40571ae91d29
okhuarobo osayuki <osayukiraymond@gmail.com>
2023-04-29 01:22:16 +0100

    created Newsletter component

---
+++ src/components/Newsletter.jsx
@@ -0,0 +1,21 @@
+import "../assets/scss/footer.scss";
+
+function Newsletter(){
+    return(
+        <div className="newsletter">
+            <div className="newsletter_top">
+                <h1 className="newsletter_top_heading">Subcribe to Our Newsletter</h1>
+                <p className="newsletter_top_paragraph">
+                    Subscribe now to get all latest updates
+                </p>
+            </div>
+
+            <div className="newsletter_bottom">
+                <input type="email" placeholder="enter your email" />
+                <button>subscribe</button>
+            </div>
+        </div>
+    )
+}
+
+export default Newsletter;
