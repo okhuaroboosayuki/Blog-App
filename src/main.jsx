9cc1553e43efbb304c01106886074c450fa93b6e
okhuarobo osayuki <osayukiraymond@gmail.com>
2023-05-14 12:53:29 +0100

    added more routes objects for other pages/routes

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,45 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import About from "./pages/About.jsx";
+import ContactPage from "./pages/Contact.jsx";
+import Category from "./pages/category/Category.jsx";
+import SingleCategory from "./pages/category/SingleCategory.jsx";
+import Blog from "./pages/blog/Blog.jsx";
+import SingleBlog from "./pages/blog/SingleBlog.jsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
   },
+  {
+    path: "/about",
+    element: <About />,
+  },
+  {
+    path: "/contact",
+    element: <ContactPage />
+  },
+  {
+    path: "/categories",
+    element: <Category />
+  },
+  {
+    path: "/categories/:categoryId",
+    element: <SingleCategory />
+  },
+  {
+    path: "/blogs",
+    element: <Blog />
+  },
+  {
+    path: "/blogs/:blogId",
+    element: <SingleBlog />
+  },
+  {
+    path: "*",
+  }
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
