bc267a9eaae920a9837af8b1bbf795b6d338f8c0
okhuarobo osayuki <osayukiraymond@gmail.com>
2023-05-16 18:32:37 +0100

    called three blog data from api with pagination

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,12 +2,18 @@ import "../assets/scss/hero.scss";
 import ArrowDownwardIcon from "@mui/icons-material/ArrowDownward";
 import { A11y, Navigation, Pagination, EffectFade } from "swiper";
 import { Swiper, SwiperSlide } from "swiper/react";
+import useFetch from "../hooks/useFetch";
 import "swiper/scss";
 import "swiper/scss/pagination";
-import 'swiper/scss/effect-fade';
+import "swiper/scss/effect-fade";
 import "animate.css";
+import { Link } from "react-router-dom";
 
 function Hero() {
+  const { loading, error, data } = useFetch(
+    "http://localhost:1337/api/blogs?populate=media&sort=createdAt:desc&pagination[page]=1&pagination[pageSize]=3"
+  );
+
   const pagination = {
     el: ".scroll_pagination_top",
     clickable: true,
@@ -29,84 +35,55 @@ function Hero() {
         }}
         className="hero"
       >
-        <SwiperSlide className="hero_wrapper">
-          <div
-            className="hero_left"
-            style={{
-              backgroundImage:
-                "url('https://res.cloudinary.com/dg0sck16v/image/upload/v1682984264/Article%20Images/alina-karpenko-03RUtFXdu1w-unsplash_ntvjo3.jpg')",
-            }}
-          ></div>
+        {loading ? (
+          <div>loading...</div>
+        ) : !loading && error ? (
+          <div>error!!!</div>
+        ) : (
+          <>
+            {data.data.map((blog) => (
+              <SwiperSlide key={blog.id} className="hero_wrapper">
+                <div
+                  className="hero_left"
+                  style={{
+                    backgroundImage: `url(http://localhost:1337${blog.attributes.media.data.attributes.url})`,
+                  }}
+                ></div>
 
-          <div className="hero_right">
-            <article className="hero_article">
-              <div className="hero_article_container">
-                <div className="hero_article_category">
-                  <span className="hero_article_category_name">
-                    <a href="#">Category 1</a>
-                  </span>
+                <div className="hero_right">
+                  <article className="hero_article">
+                    <div className="hero_article_container">
+                      <div className="hero_article_category">
+                        <span className="hero_article_category_name">
+                          <Link to={`/categories/${blog.attributes.category}`}>
+                            {blog.attributes.category}
+                          </Link>
+                        </span>
+                      </div>
+
+                      <h2 className="hero_article_title">
+                        <Link to={`/blogs/${blog.attributes.title}`}>
+                          {blog.attributes.title}
+                        </Link>
+                      </h2>
+
+                      <p className="hero_article_text">
+                        {blog.attributes.description}
+                      </p>
+
+                      <Link
+                        to={`/blogs/${blog.attributes.title}`}
+                        className="read_more"
+                      >
+                        read more
+                      </Link>
+                    </div>
+                  </article>
                 </div>
-
-                <h2 className="hero_article_title">
-                  <a href="#">
-                    Refreshing Green Smoothie Recipe with Citrus Twist
-                  </a>
-                </h2>
-
-                <p className="hero_article_text">
-                  Looking for a tasty and nutritious way to start your day? Our
-                  green smoothie recipe with a refreshing citrus twist has got
-                  you covered! This drink is packed with vitamins, minerals, and
-                  antioxidants that can help boost your immune system, support
-                  digestion, and improve your overall health.
-                </p>
-
-                <a href="#" className="read_more">
-                  read more
-                </a>
-              </div>
-            </article>
-          </div>
-        </SwiperSlide>
-
-        <SwiperSlide className="hero_wrapper">
-          <div
-            className="hero_left"
-            style={{
-              backgroundImage:
-                "url('https://res.cloudinary.com/dg0sck16v/image/upload/v1682984256/Article%20Images/tyler-nix-Y1drF0Y3Oe0-unsplash_rziml8.jpg')",
-            }}
-          ></div>
-
-          <div className="hero_right">
-            <article className="hero_article">
-              <div className="hero_article_container">
-                <div className="hero_article_category">
-                  <span className="hero_article_category_name">
-                    <a href="#">Category 2</a>
-                  </span>
-                </div>
-
-                <h2 className="hero_article_title">
-                  <a href="#">
-                    Step Up Your Cardio Game with These Stair Running Workouts.
-                  </a>
-                </h2>
-
-                <p className="hero_article_text">
-                  With stair running, you can challenge your cardiovascular
-                  system, boost your leg strength, and improve your overall
-                  fitness level. Plus, it is a low-impact workout that can be
-                  tailored to your fitness level and goals.
-                </p>
-
-                <a href="#" className="read_more">
-                  read more
-                </a>
-              </div>
-            </article>
-          </div>
-        </SwiperSlide>
+              </SwiperSlide>
+            ))}
+          </>
+        )}
 
         <div className="scroll_pagination">
           <div className="scroll_pagination_top"></div>
